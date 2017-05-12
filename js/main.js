881b5126f19f51c001b52ee8b848924260bfd112
pecorina <constanza.pecori@gmail.com>
2017-05-12 12:55:55 -0300

    intento

---
+++ js/main.js
@@ -0,0 +1,20 @@
+function formulario(nombre, apellido, email, direction, telefono){ 
+  
+  this.nombre=nombre;
+  this.apellido=apellido;
+  this.edad=edad;
+  this.email=email;
+}
+
+function botones (){
+	var nombre = document.getElementById("nombre").value;
+	var apellido = document.getElementById("apellido").value;
+    var edad = document.getElementById("edad").value;
+    var email = document.getElementById("email").value;
+
+
+}
+
+
+
+
